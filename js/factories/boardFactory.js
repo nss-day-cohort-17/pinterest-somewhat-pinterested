85d2f553d626e017b96a20bf75e1fbf1c19db031
Wisbell <westleyisbell@gmail.com>
2017-01-24 15:27:32 -0600

    Updated index.html by moving ng-app to the body tag.  Working on delete Functionality

--- js/factories/boardFactory.js
+++ js/factories/boardFactory.js
@@ -1,16 +1,13 @@
 app.factory('BoardFactory', function($http){
 
   return {
-            getUserBoardList: function(uid){
-              // https://west-doctors-patients.firebaseio.com/patients.json?orderBy="doctor_id"&equalTo="${doctor_id}"`
-              return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="uid"&equalTo="${uid}"`)
-              // `https://somewhat-pinterested.firebaseio.com/pins.json?orderBy="uid"&equalTo="${uid}"`
-              // return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&limitToFirst=2`) // this works
-                .then(function(val){
-                  console.log("val", val)
-                  return val.data
-              })
-            },
+          getUserBoardList: function(uid){
+            return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="uid"&equalTo="${uid}"`)
+              .then(function(val){
+                console.log("val", val)
+                return val.data
+            })
+          },
 
 
           createNewBoard: function(uid, boardid, url, title){
@@ -21,6 +18,12 @@ app.factory('BoardFactory', function($http){
               }
 
               $http.post('https://somewhat-pinterested.firebaseio.com/pins.json', JSON.stringify(newBoard))
+            }
+          },
+
+
+          deleteSpecificUserBoard: function(){
+
+            $delete.
           }
-        }
 })
