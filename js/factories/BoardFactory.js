09d514f58c1a21efe3ae14235a04980e62a61d7c
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-24 13:24:46 -0600

    working branch

--- js/factories/BoardFactory.js
+++ js/factories/BoardFactory.js
@@ -1,27 +1,25 @@
-  console.log("BoardFactory");
+app.factory('BoardFactory', function($http){
 
-app.factory('BoardFactory', function($http) {
   return {
-    getUserBoardList: function() {
-      return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json`)
-        // return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&limitToFirst=2`) // this works
-        .then(function(val) {
-          console.log("val", val);
-          return val.data;
-        });
-    },
+            getUserBoardList: function(){
+              // https://west-doctors-patients.firebaseio.com/patients.json?orderBy="doctor_id"&equalTo="${doctor_id}"`
+              return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&equalTo="12345"`)
+              // return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&limitToFirst=2`) // this works
+                .then(function(val){
+                  console.log("val", val)
+                  return val.data
+              })
+            },
 
 
-    createNewBoard: function(uid, boardid, url, title) {
+          createNewBoard: function(uid, boardid, url, title){
 
-      let newBoard = {
-        "uid": uid,
-        "title": title,
-        "boardid": boardid,
-        "url": url
-      };
+              let newBoard = {
+                "uid": uid,
+                "title": title
+              }
 
-      $http.post('https://somewhat-pinterested.firebaseio.com/pins.json', JSON.stringify(newBoard));
-    }
-  };
-});
+              $http.post('https://somewhat-pinterested.firebaseio.com/pins.json', JSON.stringify(newBoard))
+          }
+        }
+})
