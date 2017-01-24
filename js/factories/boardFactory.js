040ae96c47836e2513b49d989a77073f1da86e13
Wisbell <westleyisbell@gmail.com>
2017-01-24 14:35:00 -0600

    Working on specific user board partial and its functionality

--- js/factories/boardFactory.js
+++ js/factories/boardFactory.js
@@ -3,7 +3,8 @@ app.factory('BoardFactory', function($http){
   return {
             getUserBoardList: function(){
               // https://west-doctors-patients.firebaseio.com/patients.json?orderBy="doctor_id"&equalTo="${doctor_id}"`
-              return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&equalTo="12345"`)
+              return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="uid"&equalTo="${uid}"`)
+              // `https://somewhat-pinterested.firebaseio.com/pins.json?orderBy="uid"&equalTo="${uid}"`
               // return $http.get(`https://somewhat-pinterested.firebaseio.com/boards.json?orderBy="title"&limitToFirst=2`) // this works
                 .then(function(val){
                   console.log("val", val)
