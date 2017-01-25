7f9b4c800b7fd98fcaef83edc8cc4ee133923e7e
wierje <wier.je@gmail.com>
2017-01-25 15:38:32 -0600

    login functionality

--- js/factories/AuthFactory.js
+++ js/factories/AuthFactory.js
@@ -1,33 +1,48 @@
 
-app.factory('AuthFactory', function() {
+"esversion: 6";
 
-let createUser = function (userObj) {
-  return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
-    .catch(function(error) {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      // ....
-    });
-};
+ app.factory('AuthFactory', ($q) => {
+   return {
+     login (email, pass) {
+       // converts native ES6 promise to angular promise so no $scope.$apply needed
+       return $q.resolve(firebase.auth().signInWithEmailAndPassword(email, pass));
+     },
 
-let loginUser = function(userObj) {
-  console.log(userObj);
-  return firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
-  .catch(function(error) {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-  });
-};
+     getUserId () {
+       return firebase.auth().currentUser.uid;
+       console.log(currentUser.uid);
+     }
+   };
+ });
 
-let logoutUser = function() {
-  return firebase.auth().signOut();
-};
+//app.factory('AuthFactory', function() {
+// let createUser = function (userObj) {
+//   return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
+//     .catch(function(error) {
+//       let errorCode = error.code;
+//       let errorMessage = error.message;
+//       // ....
+//     });
+// };
 
-let isAuthenticated = function() {
-  return (firebase.auth().currentUser) ? true : false;
-};
+// let loginUser = function(userObj) {
+//   console.log(userObj);
+//   return firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
+//   .catch(function(error) {
+//     let errorCode = error.code;
+//     let errorMessage = error.message;
+//   });
+// };
+
+// let logoutUser = function() {
+//   return firebase.auth().signOut();
+// };
+
+// let isAuthenticated = function() {
+//   return (firebase.auth().currentUser) ? true : false;
+// };
 
 
-return {createUser, loginUser, logoutUser, isAuthenticated};
+// return {createUser, loginUser, logoutUser, isAuthenticated};
 
-});
+// });
