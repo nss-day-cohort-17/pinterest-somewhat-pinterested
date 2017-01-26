ae6829d599e1ffbf68e7c39a0b4000787b492706
Athena Perez <athenaperez@me.com>
2017-01-26 14:55:40 -0600

    styles

--- js/materializeFunction.js
+++ js/materializeFunction.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
 	console.log("materialize ready")
 	$(".dropdown-button").dropdown();
 
- 	$('select').material_select();
+ 	// $('select').material_select();
 
- 	$('.materialboxed').materialbox();
+ 	// $('.materialboxed').materialbox();
 
   $('.tooltipped').tooltip({delay: 50});
 });
+
 
