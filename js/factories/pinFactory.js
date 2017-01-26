df8eb75fdbf474f2fd18f0b262b63b5f1cd97567
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-26 09:01:45 -0600

    syncing with leave this dev

---
+++ js/factories/pinFactory.js
@@ -0,0 +1,35 @@
+app.factory('PinFactory', function($http){
+
+ return {
+           getSpecificUserPinsList: function(uid){
+             // https://west-doctors-patients.firebaseio.com/patients.json?orderBy="doctor_id"&equalTo="${doctor_id}"`)
+             // https://somewhat-pinterested.firebaseio.com/pins.json?orderBy="uid"&equalTo="${user.uid}"`)
+             return $http.get(`https://somewhat-pinterested.firebaseio.com/pins.json?orderBy="uid"&equalTo="${uid}"`)
+               .then(function(val){
+                 console.log("val", val)
+                 return val.data
+             })
+           },
+
+           createNewPin: function(uid, boardid, url, title){
+
+             let newPin = {
+               "uid": uid,
+               "boardid": boardid,
+               "url": url,
+               "title": title
+             }
+
+             $http.post('https://somewhat-pinterested.firebaseio.com/pins.json', JSON.stringify(newPin))
+         },
+
+           getAllUserPinList: function(){
+
+             return $http.get('https://somewhat-pinterested.firebaseio.com/pins.json')
+               .then(function(val){
+                 console.log("val", val)
+                 return val.data
+             })
+         }
+       }
+})
