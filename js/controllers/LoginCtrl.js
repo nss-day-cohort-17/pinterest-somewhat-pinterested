9f3438e2a5173e278845be8caaf8a5e421924634
Wisbell <westleyisbell@gmail.com>
2017-01-25 18:42:06 -0600

    A user can now log in on the log in partial.  A user can also log out, but it does not refresh the page, yet if a logged out user is still on a user pins or boards view

--- js/controllers/LoginCtrl.js
+++ js/controllers/LoginCtrl.js
@@ -1,6 +1,6 @@
 app.controller("LoginCtrl", function($scope, $location, AuthFactory){
   $scope.login = () =>  AuthFactory
   .login($scope.email, $scope.password)
-    .then(() => location.url("/"));
+    .then(() => $location.url("/"));
 
   });
