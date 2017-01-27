847eebed74e238910c8f7b9db56c21b465fb53eb
Wisbell <westleyisbell@gmail.com>
2017-01-27 08:43:01 -0600

    Users can now delete pins from their pin list

--- js/controllers/singleUserPinsCtrl.js
+++ js/controllers/singleUserPinsCtrl.js
@@ -16,8 +16,12 @@ app.controller('SingleUserPinsCtrl', function($scope, PinFactory){
     $scope.deletePinButton = function($event){
       console.log("delete button clicked")
 
-      // $scope.getElement = event.target.parentElement.parentElement.parentElement
+      $scope.getElement = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
       // $scope.getElement = $($event.target)//.closest('div')
-      console.log($scope.getElement)
+      console.log("get pin delete element", $scope.getElement)
+      $scope.deleteThisKey = $scope.getElement.classList[0]
+
+
+      PinFactory.deleteSpecificUserPin($scope.deleteThisKey)
     }
 })
